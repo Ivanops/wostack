1a4ec24798bfe2f16d3a4966fa746b9d337e3c8f
Ivan <ivanops44@gmail.com>
2015-04-21 13:20:39 -0700

    fixing code review cooments

--- Sources/your/app/Answer.java
+++ Sources/your/app/Answer.java
@@ -2,6 +2,10 @@ package your.app;
 
 import com.webobjects.foundation.NSData;
 
+/**
+ * @author Ivan
+ *
+ */
 public class Answer {
 	
 	private int points;
@@ -10,9 +14,19 @@ public class Answer {
 	public NSData img;
 	public String imagePath;
 	
+	
+	/**
+	 * Constructor
+	 */
 	public Answer(){
 		
 	}
+	
+	/**
+	 * Constructor
+	 * @param content
+	 * @param person
+	 */
 	public Answer(String content, String person) {
 		this.setContent(content);
 		this.setPerson(person);
@@ -20,47 +34,91 @@ public class Answer {
 		this.setImagePath("");
 		this.img = new NSData();
 	}
-
+	
+	/**
+	 * Gets the points of the answer
+	 * @return points of Answer
+	 */
 	public int getPoints() {
 		return points;
 	}
 
+	/**
+	 * Sets points to the answer
+	 * @param points
+	 */
 	public void setPoints(int points) {
 		this.points = points;
 	}
-
+	
+	/**
+	 * Gets the person who wrote the answer
+	 * @return person
+	 */
 	public String getPerson() {
 		return person;
 	}
 
+	/**
+	 * Sets the person name to the answer
+	 * @param person
+	 */
 	public void setPerson(String person) {
 		this.person = person;
 	}
-
+	
+	/**
+	 * Gets the answers's content
+	 * @return content
+	 */
 	public String getContent() {
 		return content;
 	}
-
+	
+	/**
+	 * Sets the answers's content
+	 * @param content
+	 */
 	public void setContent(String content) {
 		this.content = content;
 	}
 	
+	/**
+	 * Sets the answers's image path
+	 * @param newPath
+	 */
 	public void setImagePath(String newPath) {
 		this.imagePath = newPath;
 	}
 	
+	/**
+	 * Gets the image path
+	 * @return image path
+	 */
 	public String getImagePath(){
 		return this.imagePath;
 	}
 	
+	/**
+	 * Sets the image
+	 * @param img
+	 */
 	public void setImage(NSData img){
 		this.img = img;
 	}
 	
+	/**
+	 * Gets the image
+	 * @return image
+	 */
 	public NSData getImage(){
 		return this.img;
 	}
 	
+	/**
+	 * Checks if answers has an image
+	 * @return true is it does and false if not
+	 */
 	public boolean hasAnswerImage(){
 		return this.imagePath != "" ? true:false;
 	}
