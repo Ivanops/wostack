c12092e5004d9461be5ee12cb5de181cae0a4ca9
joaquin.arrazola <joaquin.arrazola@fundacion-jala.org>
2015-04-21 14:38:23 -0400

    clean code

--- Sources/your/app/Helper.java
+++ Sources/your/app/Helper.java
@@ -2,30 +2,53 @@ package your.app;
 
 import java.util.ArrayList;
 
+/**
+ * Class Helper that handle the Questions
+ *
+ */
 public class Helper {
+	
 	private static ArrayList<Question> Questions = new ArrayList<Question>();
 	private static Question SelectedQuestions;
 
+	/**
+	 * Gets the question list
+	 * @return ArrayList<Question>
+	 */
 	public static ArrayList<Question> getQuestions() {
 		return Questions;
 	}
 
+	/**
+	 * Adds a new question to the question list
+	 * @param question
+	 */
 	public static void addQuestions(Question question) {
 		Questions.add(question);
 	}
 
+	/**
+	 * Gets the question that was selected
+	 * @return Question
+	 */
 	public static Question getSelectedQuestions() {
 		return SelectedQuestions;
 	}
 
+	
+	/**
+	 * Sets the selected question
+	 * @param selectedQuestions
+	 */
 	public static void setSelectedQuestions(Question selectedQuestions) {
 		SelectedQuestions = selectedQuestions;
 	}
 	
-	public static void addAnswerToQuestions(Answer answer) {
-		SelectedQuestions.addAnswer(answer);
-	}
-
+	/**
+	 * Gets a Question from an Id
+	 * @param questionId
+	 * @return a Question if it found or null if not
+	 */
 	public static Question getQuestionsById(int questionId) {
 		for (Question question : Questions) {
 			if(question.getId() == questionId){
@@ -35,6 +58,10 @@ public class Helper {
 		return null;
 	}
 	
+	/**
+	 * Sets a Question from an Id
+	 * @param questionId
+	 */
 	public static void setSelectedQuestionsById(int questionId) {
 		Question question = getQuestionsById(questionId);
 		if(question != null){
--- Sources/your/app/components/QuestionView.java
+++ Sources/your/app/components/QuestionView.java
@@ -27,36 +27,68 @@ public class QuestionView extends WOComponent {
 	public NSData answerImage;
 	public String error;
     
+    /**
+     * Component to show the Question view
+     * @param context
+     * @throws NoSuchMethodException
+     * @throws IllegalAccessException
+     * @throws InvocationTargetException
+     */
     public QuestionView(WOContext context) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
         super(context);
         this.answerImagePath = "";
         this.answerImage = new NSData();
+        
+        //Obtain the question Id from context(Dynamic object)
         WOComponent QuestionItem =  context.component();
         Method getId = QuestionItem.getClass().getMethod("getId");
         int Id = (Integer)getId.invoke(QuestionItem);
         question = Helper.getQuestionsById(Id);
     }
 
+    /**
+     * Gets the title
+     * @return Title
+     */
     public String getTitle() {
         return question.getTitle();
     }
 
+    /**
+     * Gets the content
+     * @return Content
+     */
     public String getContent() {
         return question.getContent();
     }
     
+    /**
+     * Gets the ImagePath 
+     * @return ImagePath
+     */
     public String getImagePath() {
     	return question.getImagePath();
     }
 
+    /**
+     * Verify if has Image 
+     * @return boolean
+     */
     public boolean hasImage(){
     	return question.getImagePath() != null ? true:false;
     }
     
+    /**
+     * Gets the List of questions.
+     * @return QuestionList
+     */
     public ArrayList<Answer> getList() {
 		return question.getAnswers();
 	} 
     
+    /**
+     * Adds and Answer to a specific question
+     */
     public void addAnswer () {
     	if(this.newAnswer != null && this.person != null) {
     		Answer ans = new Answer(this.newAnswer, this.person);
@@ -91,6 +123,9 @@ public class QuestionView extends WOComponent {
 		this.answerImagePath = "";
 	}
     
+    /**
+     * Gives a point to the answer
+     */
     public void addPoint () {
     	System.out.println(this.index);
     	System.out.println("ADD");
@@ -98,6 +133,9 @@ public class QuestionView extends WOComponent {
     	aux.setPoints(aux.getPoints() + 1);
     }
     
+    /**
+     * Remove the Point the answer
+     */
     public void removePoint () {
     	System.out.println(this.index);
     	System.out.println("remove");
