b2cce8792f11e497707b7948c4745cd5714c2cb0
estivensalinas <estiven.salinas@jalasoft.com>
2015-04-24 14:59:12 -0400

    ::

--- Resources/scripts/createCollapse.js
+++ Resources/scripts/createCollapse.js
@@ -1,7 +1,23 @@
+function animate() {
+  var element = $('create-question-form');
+  var opacity = element.getOpacity() + 0.1;
+  element.setStyle({'display': 'block', 'opacity': opacity});
+  if(opacity < 1) {
+    setTimeout(animate, 50);
+  }
+}
+
+function animateShow() {
+  var element = $('create-question-form');
+  element.setStyle({'display': 'block', 'opacity': 0.0});
+  setTimeout(animate, 50);
+}
+
 function showCreate() {
 	$('create-question-form').show();
 	$('create-button').hide();
 	$('hide-button').show();
+	animateShow();
 }
 
 function hideCreate() {
